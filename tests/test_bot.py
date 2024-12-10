f1bcd0e94dd3d44c0e7b0ebac83d90e933fa29b6
Екатерина <sokolova0ekaterina@gmail.com>
2024-12-10 12:46:15 +0300

    tests

--- tests/test_bot.py
+++ tests/test_bot.py
@@ -106,6 +106,142 @@ class TestBookTelegramBot(unittest.TestCase):
         mock_update.message.reply_text.assert_called_with("Ошибка при добавлении книги.")
         self.assertEqual(result, ConversationHandler.END)
 
+    
+    def test_book_action_book_invalid(self):
+        bot = BookTelegramBot(self.bot_token)  
+        mock_update = Mock()
+        mock_context = Mock()
+        mock_catalog = Mock()
+        
+        user_id = 12345
+        mock_update.effective_user.id = user_id
+        mock_update.callback_query = Mock()
+        
+        bot._get_user_catalog = Mock(return_value=mock_catalog)
+        
+        # Книга не найдена
+        mock_update.callback_query.data = "book_nonexistent"
+        mock_catalog.get_book_by_id.return_value = None
+        
+        bot.book_action_handler(mock_update, mock_context)
+        
+        mock_update.callback_query.edit_message_text.assert_called_once_with("Книга не найдена.")
+        
+
+    def test_book_action_unread_book_success(self):
+        bot = BookTelegramBot(self.bot_token)  
+        mock_update = Mock()
+        mock_context = Mock()
+        mock_catalog = Mock()
+        
+        user_id = 12345
+        mock_update.effective_user.id = user_id
+        mock_update.callback_query = Mock()
+        
+        bot._get_user_catalog = Mock(return_value=mock_catalog)
+        
+        # Книга найдена, не прочитана
+        mock_book = Mock()
+        mock_book.is_read.return_value = False
+        mock_book.get_details.return_value = "Детали книги"
+        mock_catalog.get_book_by_id.return_value = mock_book
+        mock_update.callback_query.data = "book_123"
+        
+        bot.book_action_handler(mock_update, mock_context)
+        
+        # Check edit_message_text call
+        mock_update.callback_query.edit_message_text.assert_called_once()
+        call_args = mock_update.callback_query.edit_message_text.call_args[1]
+        
+        assert call_args['text'] == "Детали книги"
+        
+        # клавиатура
+        reply_markup = call_args['reply_markup']
+        keyboard_buttons = [button.text for row in reply_markup.inline_keyboard for button in row]
+        assert "Удалить книгу" in keyboard_buttons
+        assert "Отметить прочитанной" in keyboard_buttons
+        assert "Назад" in keyboard_buttons
+
+    
+    def test_book_action_unread_book_invalid(self):
+        bot = BookTelegramBot(self.bot_token)  
+        mock_update = Mock()
+        mock_context = Mock()
+        mock_catalog = Mock()
+        mock_book = Mock()
+        
+        user_id = 12345
+        mock_update.effective_user.id = user_id
+        mock_update.callback_query = Mock()
+        
+        bot._get_user_catalog = Mock(return_value=mock_catalog)
+        
+        # Книга найдена, прочитана
+        mock_book.is_read.return_value = True
+        mock_update.callback_query.data = "book_123"
+        
+        bot.book_action_handler(mock_update, mock_context)
+        
+        mock_update.callback_query.edit_message_text.assert_called_once()
+        call_args = mock_update.callback_query.edit_message_text.call_args[1]
+        
+        # клавиатура
+        reply_markup = call_args['reply_markup']
+        keyboard_buttons = [button.text for row in reply_markup.inline_keyboard for button in row]
+        assert "Удалить книгу" in keyboard_buttons
+        assert "Отметить прочитанной" not in keyboard_buttons
+        assert "Назад" in keyboard_buttons
+
+
+    def test_book_action_remove_book(self):
+        bot = BookTelegramBot(self.bot_token)  
+        mock_update = Mock()
+        mock_context = Mock()
+        mock_catalog = Mock()
+        
+        bot._get_user_catalog = Mock(return_value=mock_catalog)
+        
+        user_id = 12345
+        mock_update.effective_user.id = user_id
+        mock_update.callback_query = Mock()
+
+        mock_book = Mock()
+        mock_catalog.get_book_by_id = Mock(return_value=mock_book)
+
+        # Удаление книги
+        mock_update.callback_query.data = "remove_123"
+        mock_catalog.remove_book.return_value = True
+        
+        bot.book_action_handler(mock_update, mock_context)
+        bot._get_user_catalog.assert_called_once_with(user_id)
+        mock_catalog.get_book_by_id.assert_called_once_with("123")
+        mock_catalog.remove_book.assert_called_once_with("123")
+        mock_update.callback_query.edit_message_text.assert_called_once_with("Книга удалена из каталога.")
+        
+        
+    def test_book_action_remove_book_invalid(self):
+        bot = BookTelegramBot(self.bot_token)  
+        mock_update = Mock()
+        mock_context = Mock()
+        mock_catalog = Mock()
+        
+        bot._get_user_catalog = Mock(return_value=mock_catalog)
+        
+        user_id = 12345
+        mock_update.effective_user.id = user_id
+        mock_update.callback_query = Mock()
+
+        mock_book = Mock()
+        mock_catalog.get_book_by_id = Mock(return_value=mock_book)
+
+        # Удаление книги
+        mock_update.callback_query.data = "remove_123"
+        mock_catalog.remove_book.return_value = False
+        
+        bot.book_action_handler(mock_update, mock_context)
+        bot._get_user_catalog.assert_called_once_with(user_id)
+        mock_catalog.remove_book.assert_called_once_with("123")            
+
 
 if __name__ == '__main__':
     unittest.main()
